0db9634175ea751d6330c965d5ba9a8e044d7197
yalaouihicham <yalaouihicham13@gmail.com>
2022-03-17 19:42:37 +0100

    modif index.js models

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -10,35 +10,25 @@ db.message = require('../models/message.model')(sequelize, Sequelize);
 db.user = require('../models/user.model.js')(sequelize, Sequelize);
 db.comment = require('../models/comment.model.js')(sequelize, Sequelize);
 
-//foreignkey c'est une clée qui va relier deux  modeles (un utilisateur a plusieurs messages)
+//  (un utilisateur a plusieurs messages)
 db.user.hasMany(db.message, {
   onDelete: 'cascade',
-  // foreignKey: {
-  //   name: 'userId',
-  //   allowNull: false,
-  // },
-  // onUpdate: 'NO ACTION',
+});
+//  (un message a plusieurs commentaires)
+db.message.hasMany(db.comment, {
+  onDelete: 'cascade',
 });
 
-
+//(un utilisateur a plusieurs commentaires)
+db.user.hasMany(db.comment, {
+  onDelete: 'cascade',
+});
 // un message  qui appartient à un utilisateur 
-db.message.belongsTo(db.user, {
-  onDelete: 'cascade',
-  // foreignKey: 'userId'
-});
-
+db.message.belongsTo(db.user);
 //un commentaire appartient à un utilisateur 
-db.comment.belongsTo(db.user,{
-    onDelete: 'cascade',
-    foreignKey: 'userId',
-  }
-);
+db.comment.belongsTo(db.user);
 //un commentaire qui appartient à un message
-db.comment.belongsTo(db.message,{
-  onDelete: 'cascade',
-  foreignKey: 'messageId',
-});
-
+db.comment.belongsTo(db.message);
 
 
 //j'exporte mon DB//
